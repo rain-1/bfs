d7e4b34a6e661c153c5c868f988f6571f48fbb46
river <rain1@airmail.cc>
2021-11-25 21:32:59 +0000

    bfs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use std::collections::VecDeque;
 use std::env;
-use std::{fs, io};
-use std::path::{PathBuf};
+use std::fs;
 
 fn main_path() -> String {
     match env::args().nth(1) {
@@ -9,16 +9,27 @@ fn main_path() -> String {
     }
 }
 
-fn path_list_directory(path:String) -> io::Result<Vec<PathBuf>> {
-    let mut entries = fs::read_dir(path)?
-        .map(|res| res.map(|e| e.path()))
-        .collect::<Result<Vec<_>, io::Error>>()?;
-    entries.sort();
-    return Ok(entries);
+fn process_queue(mut queue: VecDeque<String>) -> () {
+    while let Some(path) = queue.pop_front() {
+        if let Ok(entries) = fs::read_dir(path) {
+            for entry in entries {
+                if let Ok(entry) = entry {
+                    if let Ok(metadata) = entry.metadata() {
+                        let path_str = entry.path().into_os_string().into_string().unwrap();
+                        if metadata.is_dir() {
+                            queue.push_back(path_str);
+                        } else {
+                            println!("{}", path_str);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 fn main() {
     let path = main_path();
-    println!("Path {}", path);
-    println!("Things: {:?}", path_list_directory(path).unwrap());
+    let queue: VecDeque<_> = VecDeque::from([path]);
+    process_queue(queue);
 }
