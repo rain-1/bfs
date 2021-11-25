c3809168eff76bbd370e02f7806a165ef4d792ef
river <rain1@airmail.cc>
2021-11-25 21:16:14 +0000

    list a directory

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::{fs, io};
+use std::path::{PathBuf};
 
 fn main_path() -> String {
     match env::args().nth(1) {
@@ -7,7 +9,16 @@ fn main_path() -> String {
     }
 }
 
+fn path_list_directory(path:String) -> io::Result<Vec<PathBuf>> {
+    let mut entries = fs::read_dir(path)?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+    entries.sort();
+    return Ok(entries);
+}
+
 fn main() {
     let path = main_path();
     println!("Path {}", path);
+    println!("Things: {:?}", path_list_directory(path).unwrap());
 }
