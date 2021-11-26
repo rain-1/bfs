efa12e6f7b4d32cc17bc910f6e42ad7cdb79335b
river <rain1@airmail.cc>
2021-11-26 15:34:18 +0000

    returns an exit code of 0 or 1 if it finds a match or not

--- readme.md
+++ readme.md
@@ -7,3 +7,5 @@
 
 Perform a breadth first search starting at the
 given path. Print filenames matching the regex.
+
+Returns an exit code of 0 if it finds a match. 1 if it does not.
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use regex::Regex;
 use std::collections::VecDeque;
 use std::env;
 use std::fs;
-use regex::Regex;
 
-fn process_queue(mut queue: VecDeque<String>, re: Regex) -> () {
+fn process_queue(mut queue: VecDeque<String>, re: Regex) -> Result<(), ()> {
+    let mut found_anything = false;
+
     while let Some(path) = queue.pop_front() {
         if let Ok(entries) = fs::read_dir(path) {
             for entry in entries {
@@ -16,21 +18,36 @@ fn process_queue(mut queue: VecDeque<String>, re: Regex) -> () {
                             queue.push_back(path_str);
                         } else if re.is_match(&filename_str) {
                             println!("{}", path_str);
+                            found_anything = true
                         }
                     }
                 }
             }
         }
     }
+
+    if found_anything {
+        Ok(())
+    }
+    else {
+        Err(())
+    }
 }
 
-fn cmdline_arg(n:usize, default:&str) -> String {
-    return env::args().nth(n).unwrap_or_else(|| String::from(default))
+fn cmdline_arg(n: usize, default: &str) -> String {
+    return env::args().nth(n).unwrap_or_else(|| String::from(default));
 }
 
-fn main() {
+fn run_app() -> Result<(), ()>  {
     let re = Regex::new(&cmdline_arg(1, ".*")).unwrap();
     let path = cmdline_arg(2, ".");
     let queue: VecDeque<_> = VecDeque::from([path]);
-    process_queue(queue, re);
+    return process_queue(queue, re);
+}
+
+fn main() {
+    std::process::exit(match run_app() {
+        Ok(_) => 0,
+        Err(_) => 1,
+    });
 }
